1c54839b03cfe97dbefaf6ef4c5cb81c5befe440
mbk <mbk@gmail.com>
2024-04-25 20:30:08 +0200

    restricted same origin policy

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    eslint: {
-        ignoreDuringBuilds: true
-    }
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  headers: [
+    {
+      key: "Access-Control-Allow-Origin",
+      value: process.env.PREPROD_PUBLIC_APP_URL,
+    },
+  ],
 };
 
 export default nextConfig;
